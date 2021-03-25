4300c74533bd1cabc8ca4376a99c3179f5d82192
Sean Unland <seanunland@Seans-MBP.lan>
2021-03-25 15:21:28 -0400

    Python Day 14

---
+++ Python Day 14/main.py
@@ -0,0 +1,15 @@
+from random import randint
+from art import logo
+from game_data import data
+
+# Display Art
+
+# Generate a random account from game_data
+
+# Format the account data into printable format
+
+# Ask user for a guess
+
+# Check if user is correct
+# Get follow count on each account
+# use if statement to check if user is correct

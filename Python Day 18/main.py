f6503ad4f8b6465716f78d8818c8c3ccd484d6dd
Sean Unland <seanunland@Seans-MacBook-Pro.local>
2021-04-06 09:55:27 -0700

    Python Day 18

---
+++ Python Day 18/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+import heroes
+
+
+
+# imports everything in module using *
+# from turtle import *
+
+# Alias modules import module name as alias name
+# import turtle as t
+
+tim = Turtle()
+tom = Turtle()
+terry = Turtle()
+
+# tim.shape("turtle")
+# tim.color("#7FFFD4")
+
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+
+
+
+print(heroes.gen())
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
